48951d482614baaee8ab9a8caf54581f032ca5bf
Philip Ganchev <phil.ganchev@graphwise.ai>
2025-09-29 20:49:01 +0300

    WIP split up compute_aggregates()

--- graphrag_eval/aggregation.py
+++ graphrag_eval/aggregation.py
@@ -1,7 +1,7 @@
 import json
 from collections import defaultdict
 from statistics import mean, median
-from typing import Any, Iterable
+from typing import Any, Collection, Iterable
 
 
 METRICS = [
@@ -100,6 +100,107 @@ def update_steps_summary_per_template(
                 pass
 
 
+def get_per_template_stats(
+    templates_ids: Collection[str],
+    number_of_samples_per_template_by_status: dict[str, dict[str, int]],
+    steps_summary_per_template: dict[str, dict[str, dict[str, int]]],
+    stats_per_template: dict[str, dict[str, dict[str, int]]],
+    step_metrics_per_template: dict[str, dict[str, dict[str, int]]],
+) -> dict[str, dict[str, Any]]:
+    summary = {}
+
+    # Add per-template stats
+    for template_id in templates_ids:
+        num_by_status = number_of_samples_per_template_by_status[template_id]
+        template_summary: dict[str, Any] = {
+            "number_of_error_samples": num_by_status["error"],
+            "number_of_success_samples": num_by_status["success"],
+        }
+        steps_summary = {
+            k1: {k2: v2 for k2, v2 in v1.items()}
+            for k1, v1 in steps_summary_per_template[template_id].items()
+        }
+        if steps_summary:
+            template_summary.update({"steps": steps_summary})
+        for metric in METRICS:
+            results_for_template = stats_per_template[template_id]
+            series = results_for_template.get(metric, [])
+            if series or metric in PROTECTED_METRICS:
+                template_summary[metric] = stats_for_series(series)
+
+        # Add step metrics for the template
+        template_step_metrics = {}
+        for metric, values in step_metrics_per_template[template_id].items():
+            template_step_metrics[metric] = stats_for_series(values)
+        if template_step_metrics:
+            template_summary["steps"].update(template_step_metrics)
+
+        summary[template_id] = template_summary
+    return summary
+
+
+def compute_micro_stats(
+    number_of_samples_per_template_by_status,
+    stats_per_template,
+    step_metrics_per_template
+):
+    values_ = number_of_samples_per_template_by_status.values()
+    micro_summary = {
+        "number_of_error_samples": sum(vals["error"] for vals in values_),
+        "number_of_success_samples": sum(vals["success"] for vals in values_),
+    }
+    for metric in METRICS:
+        series = [
+            i
+            for values in stats_per_template.values()
+            for i in values[metric]
+            if values.get(metric) is not None
+        ]
+        if series or metric in PROTECTED_METRICS:
+            micro_summary[metric] = stats_for_series(series)
+
+    # Add micro step metrics
+    micro_step_metrics = defaultdict(list)
+    for template_metrics in step_metrics_per_template.values():
+        for metric, values in template_metrics.items():
+            micro_step_metrics[metric].extend(values)
+    step_metrics = {
+        metric: stats_for_series(values)
+        for metric, values in micro_step_metrics.items()
+    }
+    micro_summary.update(step_metrics)
+    return micro_summary
+
+
+def compute_macro_stats(
+    summary_per_template: dict[str, dict[str, dict[str, dict[str, float]]]]
+) -> dict:
+    macro_summary = {}
+    for metric in METRICS:
+        means = [
+            values[metric]["mean"]
+            for template_id, values in summary_per_template.items()
+            if values.get(metric) is not None
+        ]
+        print(metric, means)
+        if means or metric in PROTECTED_METRICS:
+            macro_summary[metric] = {"mean": mean(means) if means else 0}
+
+    # Add macro step metrics
+    macro_step_metrics = defaultdict(list)
+    for template_id, template_summary in summary_per_template.items():
+        if "steps" in template_summary:
+            for metric, stats in template_summary["steps"].items():
+                if "mean" in stats:
+                    macro_step_metrics[metric].append(stats["mean"])
+    step_metrics = {
+        metric: {"mean": mean(values) if values else 0}
+        for metric, values in macro_step_metrics.items()
+    }
+    macro_summary.update(step_metrics)
+    return macro_summary
+
+
 def compute_aggregates(samples: list[dict]) -> dict:
     number_of_samples_per_template_by_status = defaultdict(lambda: defaultdict(int))
     stats_per_template = defaultdict(lambda: defaultdict(list))
@@ -129,88 +230,18 @@ def compute_aggregates(samples: list[dict]) -> dict:
             template_id
         )
 
-    summary = {"per_template": {}}
-
-    # Add per-template stats
-    for template_id in templates_ids:
-        template_summary: dict[str, Any] = {
-            "number_of_error_samples": number_of_samples_per_template_by_status[template_id]["error"],
-            "number_of_success_samples": number_of_samples_per_template_by_status[template_id]["success"],
-        }
-        steps_summary = {
-            k1: {k2: v2 for k2, v2 in v1.items()}
-            for k1, v1 in steps_summary_per_template[template_id].items()
-        }
-        if steps_summary:
-            template_summary.update({"steps": steps_summary})
-        for metric in METRICS:
-            results_for_template = stats_per_template[template_id]
-            series = results_for_template.get(metric, [])
-            if series or metric in PROTECTED_METRICS:
-                template_summary[metric] = stats_for_series(series)
-
-        # Add step metrics for the template
-        template_step_metrics = {}
-        for metric, values in step_metrics_per_template[template_id].items():
-            template_step_metrics[metric] = stats_for_series(values)
-        if template_step_metrics:
-            template_summary["steps"].update(template_step_metrics)
-
-        summary["per_template"][template_id] = template_summary
-
-    # Add micro stats
-    values_ = number_of_samples_per_template_by_status.values()
-    summary["micro"] = {
-        "number_of_error_samples": sum(
-            values["error"] for values in values_
-        ),
-        "number_of_success_samples": sum(
-            values["success"] for values in values_
-        ),
-    }
-    for metric in METRICS:
-        series = [
-            i
-            for values in stats_per_template.values()
-            for i in values[metric]
-            if values.get(metric) is not None
-        ]
-        if series or metric in PROTECTED_METRICS:
-            summary["micro"][metric] = stats_for_series(series)
-
-    # Add micro step metrics
-    micro_step_metrics = defaultdict(list)
-    for template_metrics in step_metrics_per_template.values():
-        for metric, values in template_metrics.items():
-            micro_step_metrics[metric].extend(values)
-    step_metrics = {
-        metric: stats_for_series(values)
-        for metric, values in micro_step_metrics.items()
-    }
-    summary["micro"].update(step_metrics)
-
-    # Add macro stats
-    summary["macro"] = {}
-    for metric in METRICS:
-        means = [
-            values[metric]["mean"]
-            for template_id, values in summary["per_template"].items()
-            if values.get(metric) is not None
-        ]
-        if means or metric in PROTECTED_METRICS:
-            summary["macro"][metric] = {"mean": mean(means) if means else 0}
-
-    # Add macro step metrics
-    macro_step_metrics = defaultdict(list)
-    for template_id, template_summary in summary["per_template"].items():
-        if "steps" in template_summary:
-            for metric, stats in template_summary["steps"].items():
-                if "mean" in stats:
-                    macro_step_metrics[metric].append(stats["mean"])
-    step_metrics = {
-        metric: {"mean": mean(values) if values else 0}
-        for metric, values in macro_step_metrics.items()
-    }
-    summary["macro"].update(step_metrics)
-
+    summary = {}
+    summary["per_template"] = get_per_template_stats(
+        templates_ids,
+        number_of_samples_per_template_by_status,
+        steps_summary_per_template,
+        stats_per_template,
+        step_metrics_per_template
+    )
+    summary["micro"] = compute_micro_stats(
+        number_of_samples_per_template_by_status,
+        stats_per_template,
+        step_metrics_per_template
+    )
+    summary["macro"] = compute_macro_stats(summary["per_template"])
     return summary
