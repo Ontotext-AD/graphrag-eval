625149a9cf9fbf1ef3d7f378eaed9b35a0e0095a
Neli Hateva <nelly-hateva@users.noreply.github.com>
2025-10-06 09:51:13 +0300

    Statnett-142: Fix bug in the calculation of the aggregated results in case of an output from DESCRIBE or CONSTRUCT query containing the string "results"

---
+++ tests/test_aggregation.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import yaml
+
+from graphrag_eval import compute_aggregates
+
+DATA_DIR = Path(__file__).parent / "test_data"
+
+
+def test_compute_aggregates_doesnt_throw_exception():
+    """
+    Check that `compute_aggregates()` works when the actual SPARQL query 
+    is a DESCRIBE query containing the string "results" in the text.
+    """
+    evaluation_results_file = DATA_DIR / f"evaluation_3.yaml"
+    with open(evaluation_results_file, "r", encoding="utf-8") as yaml_file:
+        per_question_eval = yaml.safe_load(yaml_file)
+        compute_aggregates(per_question_eval)
