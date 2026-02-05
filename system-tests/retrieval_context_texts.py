3fca0ccfb4384abc5e3b5f01ddf90793bd0017e3
Philip Ganchev <philip.ganchev@ontotext.com>
2026-02-04 23:31:10 -0500

    Fix system-tests/retrieval_context_texts.py
    Revert get_retrieval_evaluation_dict() call params

--- system-tests/retrieval_context_texts.py
+++ system-tests/retrieval_context_texts.py
@@ -8,7 +8,17 @@ from graphrag_eval.steps.retrieval_context_texts import (
 @pytest.mark.asyncio
 async def test_retrieval_contexts():
     result = await get_retrieval_evaluation_dict(
-        reference_answer="Rayleigh discovered that shorter wavelengths are scattered more than long wavelengths.",
+        question_text="Why is the sky blue?",
+        reference_contexts=[
+            {
+                "id": "http://example.com/resource/doc/1",
+                "text": "Rayleigh discovered that shorter wavelengths are scattered more than long wavelengths."
+            },
+            {
+                "id": "http://example.com/resource/doc/2",
+                "text": "Gases scatter sunlight"
+            }
+        ],
         actual_contexts=[
             {
                 "id": "http://example.com/resource/doc/3",
