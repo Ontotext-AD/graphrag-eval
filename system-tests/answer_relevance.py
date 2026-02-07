412ba83c3b5fe033ca40e180ae947657446e2741
Philip Ganchev <philip.ganchev@ontotext.com>
2026-02-06 20:50:58 -0500

    Rename tests-with-openai/ to tests-with-llm

--- system-tests/answer_relevance.py
+++ system-tests/answer_relevance.py
@@ -7,7 +7,7 @@ from graphrag_eval.answer_relevance import (
 )
 
 
-path = "tests-with-openai/test_data/config-llm.yaml"
+path = "tests-with-llm/test_data/config-llm.yaml"
 with open(path, encoding="utf-8") as f:
     config_dict = yaml.safe_load(f)
 config = Config(**config_dict)
